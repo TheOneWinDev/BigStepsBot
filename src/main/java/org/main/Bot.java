42d72c3b471b8afbaf39c9ce3dda92e783a4fe14
windev <asdfffdw@gmail.com>
2024-02-26 23:51:16 +0300

    version 0.0.1 bot

---
+++ src/main/java/org/main/Bot.java
@@ -0,0 +1,41 @@
+package org.main;
+
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+
+public class Bot extends TelegramLongPollingBot {
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String chatId = update.getMessage().getChatId().toString();
+            String messageText = update.getMessage().getText();
+            SendMessage message = new SendMessage();
+            message.setChatId(chatId);
+
+            if (messageText.toLowerCase().contains("погода")) {
+                message.setText(Weather.formattedWeatherData());
+            } else {
+                message.setText("BigStepsBot -> введите команду (погода):");
+            }
+
+            try {
+                execute(message);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "BOT";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "TOKEN";
+    }
+}
